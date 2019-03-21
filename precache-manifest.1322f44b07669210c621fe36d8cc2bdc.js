d33a69595c5af7bc5c01bda7fcad112e9fd41bb7
rizkygin <rizkygin@gmail.com>
2019-03-21 08:34:04 +0700

    Updates

---
+++ precache-manifest.1322f44b07669210c621fe36d8cc2bdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c779bc5e6faaf0d3f5ab",
+    "url": "/static/js/main.e2002e8b.chunk.js"
+  },
+  {
+    "revision": "300c13e89f0e1c70496a",
+    "url": "/static/js/2.6f6b556b.chunk.js"
+  },
+  {
+    "revision": "c779bc5e6faaf0d3f5ab",
+    "url": "/static/css/main.2bbe8973.chunk.css"
+  },
+  {
+    "revision": "f7cac8db282ec19291aa746bcf2640a5",
+    "url": "/index.html"
+  }
+];
